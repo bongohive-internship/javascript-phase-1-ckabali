7b00acbabbe045cb6488c616601354dd8e67bcba
collins kabali <51477747+ckabali@users.noreply.github.com>
2019-06-14 13:34:25 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -5,6 +5,15 @@
     console.log('You are either ' + age + ' or ' + (age - 1));
 }
 
+function calculateSupply(age, numPerDay) {
+  var maxAge = 100;
+  var totalNeeded = (numPerDay * 365) * (maxAge - age);
+  var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+  console.log(message);
+}
+
+
+
 
 
 
