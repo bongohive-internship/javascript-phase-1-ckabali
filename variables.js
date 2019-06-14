548bd1d9145447e6ec9aa8a83d9c1f8499313794
collins kabali <51477747+ckabali@users.noreply.github.com>
2019-06-14 09:43:35 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -3,3 +3,11 @@ Var birthYear = 1995;
 Var futureDate = 2019;
 Var possibleAge = futureDate - birthYear;
 
+var currentAge = 24;
+Var maximumAge = 85;
+Number estimatedAmountPerDay = 20;
+TotalEstimatedToEat = estimatedAmountPerDay× (maximumAge - currentAge);
+system.out.println(String:"You will need NN to last you until the ripe old age of X".);
+
+
+
